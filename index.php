a6089e75e0b6472d519bc6d92ba453062cce37df
yaser <yassersharifi74@gmail.com>
2021-09-12 11:11:20 +0430

    install shabnam font

--- index.php
+++ index.php
@@ -7,7 +7,13 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-gXt9imSW0VcJVHezoNQsP+TNrjYXoGcrqBZJpry9zJt8PCQjobwmhMGaDHTASo9N" crossorigin="anonymous">
-
+    <!-- Shabnam Font CDN -->
+    <link href='https://cdn.fontcdn.ir/Font/Persian/Shabnam/Shabnam.css' rel='stylesheet' type='text/css'>
+    <style>
+      html, body {
+        font-family: Shabnam !important;
+      }
+    </style>
     <title>Crud Application Vue</title>
   </head>
   <body>
